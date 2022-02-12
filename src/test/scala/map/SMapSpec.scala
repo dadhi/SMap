47bad991bd1aefbb18e9c0f9e3401b78ca224cca
dadhi <max.volkov@gmail.com>
2022-02-12 09:53:08 +0100

    added test for apply

--- src/test/scala/map/SMapSpec.scala
+++ src/test/scala/map/SMapSpec.scala
@@ -8,4 +8,13 @@ class SMapSpec extends AnyFunSuite {
   test("Empty map isEmpty") {
     assert(SMap.empty.isEmpty)
   }
+  test("Map of one item is not Empty") {
+    assert(Map(1 -> 2).isEmpty == false)
+  }
+  test("Maps of two equal items are equal") {
+    assert(Map("a" -> 2) == Map("a" -> 2))
+  }
+  test("Maps of two items is not equal to the Map of one") {
+    assert(Map("a" -> 2, "b" -> 3) != Map("a" -> 2))
+  }
 }
