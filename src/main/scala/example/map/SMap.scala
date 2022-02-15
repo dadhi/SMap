5b370ce34ba4ca7ba655975eefa5840e4a66b878
dadhi <max.volkov@gmail.com>
2022-02-15 16:57:14 +0100

    completed B3

--- src/main/scala/example/map/SMap.scala
+++ src/main/scala/example/map/SMap.scala
@@ -961,93 +961,120 @@ object SMap {
       else Branch3(left, e0, mid, newEntry, right)
     }
 
-    /*
-            internal override ImhMap<K, V> RemoveEntry(Entry removedEntry)
-            {
-                var midLeft  = Entry0;
-                var middle   = Middle;
-                var midRight = e1;
-                var right    = Right;
-
-                // case 1, downward: swap the predecessor entry (max left entry) with the mid entry, then proceed to remove the predecessor from the Left branch
-                if (removedEntry == midLeft)
-                    removedEntry = midLeft = Left.GetMaxHashEntryOrDefault();
-
-                if (removedEntry.Hash <= midLeft.Hash)
-                {
-                    var newLeft = Left.RemoveEntry(removedEntry);
-                    if (newLeft == Empty)
-                    {
-                        if (middle is Leaf5Plus1Plus1 == false)
-                            return new Branch2(middle.AddOrGetEntry(midLeft.Hash, midLeft), midRight, right); //! the height does not change
-                        return new Branch3(midLeft, removedEntry = middle.GetMinHashEntryOrDefault(), middle.RemoveEntry(removedEntry), midRight, right); //! the height does not change
-                    }
-
-                    // rebalance is needed because the branch was merged from Br2 to Br3 or to Leaf and the height decrease
-                    if (Left is Branch2 && newLeft is Branch2 == false)
-                    {
-                        // the hole has a 3-node as a parent and a 3-node as a sibling.
-                        if (middle is Branch3 mb3) //! the height does not change
-                            return new Branch3(new Branch2(newLeft, midLeft, mb3.Left), mb3.Entry0, new Branch2(mb3.Middle, mb3.Entry1, mb3.Right), midRight, right);
-
-                        // the hole has a 3-node as a parent and a 2-node as a sibling.
-                        var mb2 = (Branch2)middle;
-                        return new Branch2(new Branch3(newLeft, midLeft, mb2.Left, mb2.MidEntry, mb2.Right), midRight, right);
-                    }
-
-                    return new Branch3(newLeft, midLeft, middle, midRight, right); // no rebalance needed
-                }
-
-                if (removedEntry == midRight)
-                    removedEntry = midRight = middle.GetMaxHashEntryOrDefault();
-
-                if (removedEntry.Hash <= midRight.Hash)
-                {
-                    var newMiddle = middle.RemoveEntry(removedEntry);
-                    if (newMiddle == Empty)
-                    {
-                        if (right is Leaf5Plus1Plus1 == false)
-                            return new Branch2(Left, midLeft, right.AddOrGetEntry(midLeft.Hash, midLeft)); // the Br3 become the Br2 but the height did not change - so no rebalance needed
-                        return new Branch3(Left, midLeft, midRight, removedEntry = right.GetMinHashEntryOrDefault(), right.RemoveEntry(removedEntry)); //! the height does not change
-                    }
-
-                    if (middle is Branch2 && newMiddle is Branch2 == false)
-                    {
-                        // the hole has a 3-node as a parent and a 3-node as a sibling.
-                        if (right is Branch3 rb3) //! the height does not change
-                            return new Branch3(Left, midLeft, new Branch2(newMiddle, midRight, rb3.Left), rb3.Entry0, new Branch2(rb3.Middle, rb3.Entry1, rb3.Right));
-
-                        // the hole has a 3-node as a parent and a 2-node as a sibling.
-                        var rb2 = (Branch2)right;
-                        return new Branch2(Left, midLeft, new Branch3(newMiddle, midRight, rb2.Left, rb2.MidEntry, rb2.Right));
-                    }
-
-                    return new Branch3(Left, midLeft, newMiddle, midRight, right);
-                }
-
-                var newRight = right.RemoveEntry(removedEntry);
-                if (newRight == Empty)
-                {
-                    if (middle is Leaf5Plus1Plus1 == false)
-                        return new Branch2(Left, midLeft, middle.AddOrGetEntry(midRight.Hash, midRight));
-                    return new Branch3(Left, midLeft, middle.RemoveEntry(removedEntry = middle.GetMaxHashEntryOrDefault()), removedEntry, midRight);
-                }
-
-                // right was a Br2 but now is Leaf or Br3 - means the branch height is decrease
-                if (right is Branch2 && newRight is Branch2 == false)
-                {
-                    // the hole has a 3-node as a parent and a 3-node as a sibling.new
-                    if (middle is Branch3 mb3) //! the height does not change
-                        return new Branch3(Left, midLeft, new Branch2(mb3.Left, mb3.Entry0, mb3.Middle), mb3.Entry1, new Branch2(mb3.Right, midRight, newRight));
-
-                    // the hole has a 3-node as a parent and a 2-node as a sibling.
-                    var mb2 = (Branch2)middle;
-                    return new Branch2(Left, midLeft, new Branch3(mb2.Left, mb2.MidEntry, mb2.Right, midRight, newRight));
-                }
-
-                return new Branch3(Left, midLeft, middle, midRight, newRight);
+    override def removeEntry(entry: Entry[K, V]) = {
+      // case 1 downward:
+      // swap the predecessor entry (max left entry) with the mid entry,
+      // then proceed to remove the predecessor from the Left branch
+      val h = entry.hash; var h0 = e0.hash; var h1 = e1.hash
+      if (h <= h0) {
+        val leftMaxEntry = if (h == h0) left.getMaxHashEntryOrNull else e0
+        val newLeft = left.removeEntry(if (h == h0) leftMaxEntry else entry)
+        left match {
+          case _: Entry[K, V] =>
+            mid match {
+              case _: Leaf5PlusPlus[K, V] => {
+                val midLeftEntry = mid.getMinHashEntryOrNull
+                // todo: @fix what if mid is Entry (it is not the case for Branch2, otherwise it would be a Leaf)?
+                val newMid = mid.removeEntry(midLeftEntry)
+                Branch3(leftMaxEntry, midLeftEntry, newMid, e1, right)
+              }
+              case _ =>
+                Branch2(mid.addOrGetEntry(leftMaxEntry), e1, right)
             }
-     */
-
+          // rebalance is needed because the branch was merged
+          // from Br2 to Br3 or to Leaf and the height decreased
+          case _: Branch2[K, V] if (!newLeft.isInstanceOf[Branch2[K, V]]) =>
+            mid match {
+              // the hole has a 3-node as a parent and a 3-node as a sibling.
+              case b3: Branch3[K, V] =>
+                Branch3(
+                  Branch2(newLeft, leftMaxEntry, b3.left),
+                  b3.e0,
+                  Branch2(b3.mid, b3.e1, b3.right),
+                  e1,
+                  right
+                )
+              // the hole has a 3-node as a parent and a 2-node as a sibling.
+              case b2: Branch2[K, V] =>
+                Branch2(
+                  Branch3(newLeft, leftMaxEntry, b2.left, b2.e, b2.right),
+                  e1,
+                  right
+                )
+            }
+          case _ =>
+            Branch3(newLeft, leftMaxEntry, mid, e1, right)
+        }
+      } else if (h <= h1) {
+        val midMaxEntry = if (h == h1) mid.getMaxHashEntryOrNull else e1
+        val newMid = mid.removeEntry(if (h == h1) midMaxEntry else entry)
+        mid match {
+          case _: Entry[K, V] =>
+            right match {
+              case _: Leaf5PlusPlus[K, V] => {
+                val rightMinEntry = right.getMinHashEntryOrNull
+                // todo: @fix what if right is Entry (it is not the case for Branch2, otherwise it would be a Leaf)?
+                val newRight = right.removeEntry(rightMinEntry)
+                Branch3(left, e0, midMaxEntry, rightMinEntry, newRight)
+              }
+              case _ =>
+                Branch2(left, e0, right.addOrGetEntry(midMaxEntry))
+            }
+          case _: Branch2[K, V] if (!newMid.isInstanceOf[Branch2[K, V]]) =>
+            right match {
+              // the hole has a 3-node as a parent and a 3-node as a sibling.
+              case b3: Branch3[K, V] =>
+                Branch3(
+                  left,
+                  e0,
+                  Branch2(newMid, midMaxEntry, b3.left),
+                  b3.e0,
+                  Branch2(b3.mid, b3.e1, b3.right)
+                )
+              // the hole has a 3-node as a parent and a 2-node as a sibling.
+              case b2: Branch2[K, V] =>
+                Branch2(
+                  left,
+                  e0,
+                  Branch3(newMid, midMaxEntry, b2.left, b2.e, b2.right)
+                )
+            }
+          case _ => Branch3(left, e0, newMid, midMaxEntry, right)
+        }
+      } else {
+        val newRight = right.removeEntry(entry)
+        right match {
+          case _: Entry[K, V] =>
+            mid match {
+              case _: Leaf5PlusPlus[K, V] => {
+                val midMaxEntry = mid.getMaxHashEntryOrNull
+                // todo: @fix what if mid is Entry (it is not the case for Branch2, otherwise it would be a Leaf)?
+                val newMid = mid.removeEntry(midMaxEntry)
+                Branch3(left, e0, newMid, midMaxEntry, e1)
+              }
+              case _ => Branch2(left, e0, mid.addOrGetEntry(e1))
+            }
+          case _: Branch2[K, V] if (!newRight.isInstanceOf[Branch2[K, V]]) =>
+            mid match {
+              case b3: Branch3[K, V] =>
+                Branch3(
+                  left,
+                  e0,
+                  Branch2(b3.left, b3.e0, b3.mid),
+                  b3.e1,
+                  Branch2(b3.right, e1, newRight)
+                )
+              case b2: Branch2[K, V] =>
+                Branch2(
+                  left,
+                  e0,
+                  Branch3(b2.left, b2.e, b2.right, e1, newRight)
+                )
+            }
+          case _ =>
+            Branch3(left, e0, mid, e1, newRight)
+        }
+      }
+    }
   }
 }
