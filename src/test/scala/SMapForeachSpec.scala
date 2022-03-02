3ab266a735e46198fd5355cf55f64f2b61bae90b
dadhi <max.volkov@gmail.com>
2022-03-02 15:19:41 +0100

    fix test name

--- src/test/scala/SMapForeachSpec.scala
+++ src/test/scala/SMapForeachSpec.scala
@@ -3,7 +3,7 @@ import speedy.SMap
 import org.scalatest.funsuite._
 
 class SMapForeachSpec extends AnyFunSuite {
-  test("Empty map is empty map") {
+  test("Foreach of empty map ends with nothing") {
     val m = SMap.empty[Int, String]
     val l = ListBuffer.empty[Int]
     m.foreach(()) { (_, _, e) => l += e.hash }
