ffc3a981b1542930fed23ef096de9f32fe892cc3
dadhi <max.volkov@gmail.com>
2022-03-04 12:42:44 +0100

    more tests for foreach

--- src/test/scala/ForeachCheck.scala
+++ src/test/scala/ForeachCheck.scala
@@ -7,8 +7,19 @@ import speedy.SMap
 
 object ForeachCheck extends Properties("SMap") {
 
-  property("Input and foreach output list of Leaf-based map should be the same") = {
-    val sizes = Gen.choose(1, 30)
+  property("Input and output list of tiny Leaf-based map should be the same") = {
+    val sizes = Gen.choose(1, 7)
+    forAll (sizes) { (size: Int) =>
+      val keys = (1 to size).toVector
+      val map = SMap(keys.map((_, '!')): _*)
+      val ks = ArrayBuffer.empty[Int]
+      map.foreach(ks) { (ks, _, e) => ks += e.key }
+      ks == keys
+    }
+  }
+
+  property("Input and output list of big Branch-based map should be the same") = {
+    val sizes = Gen.choose(8, 100)
     forAll (sizes) { (size: Int) =>
       val keys = (1 to size).toVector
       val map = SMap(keys.map((_, '!')): _*)
