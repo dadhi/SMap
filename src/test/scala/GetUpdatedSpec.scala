740be24f668058ec8bbea95335be5225695acd22
dadhi <max.volkov@gmail.com>
2022-03-09 16:04:00 +0100

    full sporadic test

--- src/test/scala/GetUpdatedSpec.scala
+++ src/test/scala/GetUpdatedSpec.scala
@@ -55,130 +55,197 @@ class GetUpdatedSpec extends AnyFunSuite with Matchers {
     mr = m.removed(2)
     mr.getOrElse(1, null) shouldBe "a"
     mr.size shouldBe 1
-    
-    // Assert.AreSame(m, );
-    // Assert.AreSame(m, m.Remove(0));
-    // mr = m.Remove(2);
-    // Assert.AreEqual("a", mr.GetValueOrDefault(1));
-    // Assert.AreEqual(1, mr.Count());
 
-    // m = m.AddOrUpdate(3, "c");
-    // Assert.AreEqual("c",  m.GetValueOrDefault(3));
-    // Assert.AreEqual("b",  m.GetValueOrDefault(2));
-    // Assert.AreEqual("a",  m.GetValueOrDefault(1));
-    // Assert.AreEqual(null, m.GetValueOrDefault(10));
-    // CollectionAssert.AreEquivalent(new[] { 1, 2, 3 }, m.Enumerate().Select(x => x.Hash));
-    // Assert.AreEqual(3, m.Count());
+    m = m.updated(3, "c");
+    m.toList shouldBe List(1 -> "a", 2 -> "b", 3 -> "c")
 
-    // Assert.AreSame(m, m.AddOrKeep(3, "aa").AddOrKeep(2, "bb").AddOrKeep(1, "cc"));
-    // Assert.AreSame(m, m.Remove(0));
-    // mr = m.Remove(2);
-    // Assert.AreEqual("a", mr.GetValueOrDefault(1));
-    // Assert.AreEqual("c", mr.GetValueOrDefault(3));
-    // Assert.AreEqual(2, mr.Count());
+    m.updatedOrKept(3, "aa")
+      .updatedOrKept(2, "bb")
+      .updatedOrKept(1, "cc") shouldBe m
+    m.removed(0) shouldBe m
+    mr = m.removed(2)
+    mr.getOrElse(1, null) shouldBe "a"
+    mr.getOrElse(3, null) shouldBe "c"
+    mr.size shouldBe 2
 
-    // m = m.AddOrUpdate(4, "d");
-    // Assert.AreEqual("c",  m.GetValueOrDefault(3));
-    // Assert.AreEqual("b",  m.GetValueOrDefault(2));
-    // Assert.AreEqual("a",  m.GetValueOrDefault(1));
-    // Assert.AreEqual("d",  m.GetValueOrDefault(4));
-    // Assert.AreEqual(null, m.GetValueOrDefault(10));
-    // CollectionAssert.AreEquivalent(new[] { 1, 2, 3, 4 }, m.Enumerate().Select(x => x.Hash));
-    // Assert.AreEqual(4, m.Count());
+    m = m.updated(4, "d")
+    m.size shouldBe 4
+    m.toList shouldBe List(1 -> "a", 2 -> "b", 3 -> "c", 4 -> "d")
 
-    // Assert.AreSame(m, m.AddOrKeep(3, "aa").AddOrKeep(2, "bb").AddOrKeep(1, "cc"));
-    // Assert.AreSame(m, m.Remove(0));
+    m.updatedOrKept(3, "aa")
+      .updatedOrKept(2, "bb")
+      .updatedOrKept(1, "cc") shouldBe m
+    m.removed(0) shouldBe m
 
-    // m = m.AddOrUpdate(5, "e");
-    // Assert.AreEqual("c",  m.GetValueOrDefault(3));
-    // Assert.AreEqual("b",  m.GetValueOrDefault(2));
-    // Assert.AreEqual("a",  m.GetValueOrDefault(1));
-    // Assert.AreEqual("d",  m.GetValueOrDefault(4));
-    // Assert.AreEqual("e",  m.GetValueOrDefault(5));
-    // Assert.AreEqual(null, m.GetValueOrDefault(10));
-    // CollectionAssert.AreEquivalent(new[] { 1, 2, 3, 4, 5 }, m.Enumerate().Select(x => x.Hash));
-    // Assert.AreEqual(5, m.Count());
+    m = m.updated(5, "e")
+    m.size shouldBe 5
+    m.toList shouldBe List(1 -> "a", 2 -> "b", 3 -> "c", 4 -> "d", 5 -> "e")
 
-    // Assert.AreSame(m, m.AddOrKeep(3, "aa").AddOrKeep(2, "bb").AddOrKeep(1, "cc"));
-    // Assert.AreSame(m, m.Remove(0));
+    m.updatedOrKept(3, "aa")
+      .updatedOrKept(2, "bb")
+      .updatedOrKept(1, "cc") shouldBe m
+    m.removed(0) shouldBe m
 
-    // m = m.AddOrUpdate(6, "6");
-    // Assert.AreEqual("6",  m.GetValueOrDefault(6));
-    // Assert.AreEqual("e",  m.GetValueOrDefault(5));
-    // Assert.AreEqual("d",  m.GetValueOrDefault(4));
-    // Assert.AreEqual("c",  m.GetValueOrDefault(3));
-    // Assert.AreEqual("b",  m.GetValueOrDefault(2));
-    // Assert.AreEqual("a",  m.GetValueOrDefault(1));
-    // Assert.AreEqual(null, m.GetValueOrDefault(10));
-    // Assert.AreSame(m, m.AddOrKeep(3, "aa").AddOrKeep(2, "bb").AddOrKeep(1, "cc"));
-    // CollectionAssert.AreEquivalent(new[] { 1, 2, 3, 4, 5, 6 }, m.Enumerate().Select(x => x.Hash));
-    // Assert.AreEqual(6, m.Count());
+    m = m.updated(6, "6")
+    m.size shouldBe 6
+    m.toList shouldBe List(
+      1 -> "a",
+      2 -> "b",
+      3 -> "c",
+      4 -> "d",
+      5 -> "e",
+      6 -> "6"
+    )
 
-    // m = m.AddOrUpdate(7, "7");
-    // Assert.AreEqual("7",  m.GetValueOrDefault(7));
-    // m = m.AddOrUpdate(8, "8");
-    // Assert.AreEqual("8",  m.GetValueOrDefault(8));
-    // m = m.AddOrUpdate(9, "9");
-    // Assert.AreEqual("9",  m.GetValueOrDefault(9));
-    // CollectionAssert.AreEquivalent(new[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 }, m.Enumerate().Select(x => x.Hash));
-    // Assert.AreEqual(9, m.Count());
+    m = m.updated(7, "7").updated(8, "8").updated(9, "9")
+    m.size shouldBe 9
+    m.toList shouldBe List(
+      1 -> "a",
+      2 -> "b",
+      3 -> "c",
+      4 -> "d",
+      5 -> "e",
+      6 -> "6",
+      7 -> "7",
+      8 -> "8",
+      9 -> "9"
+    )
 
-    // m = m.AddOrUpdate(10, "10");
-    // Assert.AreEqual("10", m.GetValueOrDefault(10));
-    // Assert.AreEqual("9",  m.GetValueOrDefault(9));
-    // Assert.AreEqual("8",  m.GetValueOrDefault(8));
-    // Assert.AreEqual("7",  m.GetValueOrDefault(7));
-    // Assert.AreEqual("6",  m.GetValueOrDefault(6));
-    // Assert.AreEqual("e",  m.GetValueOrDefault(5));
-    // Assert.AreEqual("d",  m.GetValueOrDefault(4));
-    // Assert.AreEqual("c",  m.GetValueOrDefault(3));
-    // Assert.AreEqual("b",  m.GetValueOrDefault(2));
-    // Assert.AreEqual("a",  m.GetValueOrDefault(1));
-    // Assert.AreEqual(null, m.GetValueOrDefault(11));
-    // Assert.AreSame(m, m.AddOrKeep(8, "8!").AddOrKeep(5, "5!").AddOrKeep(3, "aa").AddOrKeep(2, "bb").AddOrKeep(1, "cc"));
-    // CollectionAssert.AreEquivalent(new[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 }, m.Enumerate().Select(x => x.Hash));
-    // Assert.AreEqual(10, m.Count());
+    m = m.updated(10, "10")
+    assert(Some("10") == m.get(10))
+    assert(Some("9") == m.get(9))
+    assert(Some("8") == m.get(8))
+    assert(Some("7") == m.get(7))
+    assert(Some("6") == m.get(6))
+    assert(Some("e") == m.get(5))
+    assert(Some("d") == m.get(4))
+    assert(Some("c") == m.get(3))
+    assert(Some("b") == m.get(2))
+    assert(Some("a") == m.get(1))
+    assert(None == m.get(11))
+    assert(
+      m == m
+        .updatedOrKept(8, "8!")
+        .updatedOrKept(5, "5!")
+        .updatedOrKept(3, "aa")
+        .updatedOrKept(2, "bb")
+        .updatedOrKept(1, "cc")
+    )
+    m.size shouldBe 10
+    m.toList shouldBe List(
+      1 -> "a",
+      2 -> "b",
+      3 -> "c",
+      4 -> "d",
+      5 -> "e",
+      6 -> "6",
+      7 -> "7",
+      8 -> "8",
+      9 -> "9",
+      10 -> "10"
+    )
 
-    // m = m.AddOrUpdate(11, "11");
-    // m = m.AddOrUpdate(12, "12");
-    // m = m.AddOrUpdate(13, "13");
-    // Assert.AreEqual("11",  m.GetValueOrDefault(11));
-    // Assert.AreEqual("12",  m.GetValueOrDefault(12));
-    // Assert.AreEqual("13",  m.GetValueOrDefault(13));
-    // CollectionAssert.AreEquivalent(new[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13 }, m.Enumerate().Select(x => x.Hash));
-    // Assert.AreEqual(13, m.Count());
+    m = m.updated(11, "11").updated(12, "12").updated(13, "13")
+    m.size shouldBe 13
+    m.toList shouldBe List(
+      1 -> "a",
+      2 -> "b",
+      3 -> "c",
+      4 -> "d",
+      5 -> "e",
+      6 -> "6",
+      7 -> "7",
+      8 -> "8",
+      9 -> "9",
+      10 -> "10",
+      11 -> "11",
+      12 -> "12",
+      13 -> "13"
+    )
 
-    // m = m.AddOrUpdate(14, "14");
-    // Assert.AreEqual("14",  m.GetValueOrDefault(14));
-    // Assert.AreEqual(14, m.Count());
+    m = m.updated(14, "14");
+    assert("14" == m.getOrElse(14, null))
+    assert(14 == m.size)
 
-    // m = m.AddOrUpdate(15, "15");
-    // m = m.AddOrUpdate(16, "16");
-    // m = m.AddOrUpdate(17, "17");
-    // Assert.AreEqual("15",  m.GetValueOrDefault(15));
-    // Assert.AreEqual("16",  m.GetValueOrDefault(16));
-    // Assert.AreEqual("17",  m.GetValueOrDefault(17));
-    // CollectionAssert.AreEquivalent(new[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17 }, m.Enumerate().Select(x => x.Hash));
-    // Assert.AreEqual(17, m.Count());
+    m = m.updated(15, "15").updated(16, "16").updated(17, "17")
+    assert(Some("15") == m.get(15))
+    assert(Some("16") == m.get(16))
+    assert(Some("17") == m.get(17))
+    assert(17 == m.size)
+    m.toList shouldBe List(
+      1 -> "a",
+      2 -> "b",
+      3 -> "c",
+      4 -> "d",
+      5 -> "e",
+      6 -> "6",
+      7 -> "7",
+      8 -> "8",
+      9 -> "9",
+      10 -> "10",
+      11 -> "11",
+      12 -> "12",
+      13 -> "13",
+      14 -> "14",
+      15 -> "15",
+      16 -> "16",
+      17 -> "17"
+    )
 
-    // m = m.AddOrUpdate(18, "18");
-    // Assert.AreEqual("18",  m.GetValueOrDefault(18));
-    // CollectionAssert.AreEquivalent(new[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18 }, m.Enumerate().Select(x => x.Hash));
-    // Assert.AreEqual(18, m.Count());
+    m = m.updated(18, "18")
+    assert("18" == m.getOrElse(18, null))
+    assert(18 == m.size)
+    m.toList shouldBe List(
+      1 -> "a",
+      2 -> "b",
+      3 -> "c",
+      4 -> "d",
+      5 -> "e",
+      6 -> "6",
+      7 -> "7",
+      8 -> "8",
+      9 -> "9",
+      10 -> "10",
+      11 -> "11",
+      12 -> "12",
+      13 -> "13",
+      14 -> "14",
+      15 -> "15",
+      16 -> "16",
+      17 -> "17",
+      18 -> "18"
+    )
 
-    // var r = m.Remove(18).Remove(17).Remove(16);
-    // CollectionAssert.AreEquivalent(new[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 }, r.Enumerate().Select(x => x.Hash));
-    // Assert.IsNull(r.GetValueOrDefault(16));
+    var r = m.removed(18).removed(17).removed(16)
+    assert(None == r.get(16))
+    r.toList shouldBe List(
+      1 -> "a",
+      2 -> "b",
+      3 -> "c",
+      4 -> "d",
+      5 -> "e",
+      6 -> "6",
+      7 -> "7",
+      8 -> "8",
+      9 -> "9",
+      10 -> "10",
+      11 -> "11",
+      12 -> "12",
+      13 -> "13",
+      14 -> "14",
+      15 -> "15"
+    )
 
-    // var rr = r.Remove(16);
-    // Assert.AreSame(r, rr);
+    m = m.updated(18, "18").updatedOrKept(18, "18")
+    assert("18" == m.getOrElse(18, null))
 
-    // m = m.AddOrUpdate(18, "18");
-    // m = m.AddOrKeep(18, "18");
-    // Assert.AreEqual("18",  m.GetValueOrDefault(18));
-
-    // m = m.AddOrUpdate(19, "19").AddOrUpdate(20, "20").AddOrUpdate(21, "21").AddOrUpdate(22, "22").AddOrUpdate(23, "23");
-    // rr = m.Remove(25).Remove(21);
-    // Assert.IsNull(rr.GetValueOrDefault(21));
+    m = m
+      .updated(19, "19")
+      .updated(20, "20")
+      .updated(21, "21")
+      .updated(22, "22")
+      .updated(23, "23")
+    r = m.removed(25).removed(21)
+    assert(r.get(21).isEmpty)
   }
 }
